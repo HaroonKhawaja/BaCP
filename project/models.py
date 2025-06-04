40bb8c1a8e1df2ddfce5544b62653c42f524227d
HaroonKhawaja <HaroonKhawaja>
2025-06-04 05:03:51 +0000

    squad dataset implementation

--- project/models.py
+++ project/models.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet50, resnet101, vgg11, vgg19, vit_b_16, vit_l_16
 from torchvision.models import ResNet50_Weights, ResNet101_Weights, VGG11_Weights, VGG19_Weights, ViT_B_16_Weights, ViT_L_16_Weights
-from transformers import AutoModelForCausalLM, AutoConfig
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, AutoModelForQuestionAnswering, AutoTokenizer
 
 from functools import lru_cache
 from utils import freeze_weights
@@ -17,12 +16,12 @@ MODEL_SPECS = {
     "vgg19":     {"fn": vgg19,     "dim": 4096, "weight": VGG19_Weights.IMAGENET1K_V1,     "type": "vision", "family": "vgg"},
     "vitb16":    {"fn": vit_b_16,  "dim": 768,  "weight": ViT_B_16_Weights.IMAGENET1K_V1,  "type": "vision", "family": "vit"},
     "vitl16":    {"fn": vit_l_16,  "dim": 1024, "weight": ViT_L_16_Weights.IMAGENET1K_V1,  "type": "vision", "family": "vit"},
-    "distilbert-base-uncased": {"fn": AutoModelForSequenceClassification, "dim": 768, "type": "language", "family": "bert"},
-    "roberta-base": {"fn": AutoModelForSequenceClassification, "dim": 768, "type": "language", "family": "bert"}
+    "distilbert-base-uncased": {"dim": 768, "type": "language", "family": "bert"},
+    "roberta-base": {"dim": 768, "type": "language", "family": "bert"}
 }
 
 @lru_cache(maxsize=None)
-def get_model_components(model_name, pretrained=True, num_llm_labels=2):
+def get_model_components(model_name, pretrained=True, num_llm_labels=2, model_task='cls'):
     if model_name not in MODEL_SPECS:
         raise ValueError(f"Unknown model: '{model_name}'. Available: {list(MODEL_SPECS.keys())}")
 
@@ -31,7 +30,11 @@ def get_model_components(model_name, pretrained=True, num_llm_labels=2):
         weights = spec["weight"] if pretrained else None
         model = spec["fn"](weights=weights)
     else:
-        model = spec["fn"].from_pretrained(model_name, num_labels=num_llm_labels)
+        if model_task == 'squad':
+            model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+        else:
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_llm_labels)
+        
 
     return {
         "model": model,
@@ -91,15 +94,16 @@ class EncoderProjectionNetwork(nn.Module):
             return x
 
 class ClassificationNetwork(nn.Module):
-    def __init__(self, model_name, num_classes, freeze=False, adapt_for_cifar10=True):
+    def __init__(self, model_name, num_classes=10, freeze=False, adapt_for_cifar10=True, model_task='cls'):
         super().__init__()
         
         # Loading model components
-        components = get_model_components(model_name, num_llm_labels=num_classes)
+        components = get_model_components(model_name, num_llm_labels=num_classes, model_task=model_task)
         self.model = components["model"]
         self.embedding_dim = components["embedding_dim"]
         self.model_type = components["model_type"]
         self.model_family = components["model_family"]
+        self.model_task = model_task
         
         # Freezing the model if applicable
         if freeze:
@@ -118,8 +122,11 @@ class ClassificationNetwork(nn.Module):
         if self.model_type == "vision":
             return self.model(x)
         else:
-            return self.model(
-                input_ids=x["input_ids"],
-                attention_mask=x["attention_mask"],
-                labels=x["label"]
-            )
+            if self.model_task == 'squad':
+                return self.model(**x)
+            else:
+                return self.model(
+                    input_ids=x["input_ids"],
+                    attention_mask=x["attention_mask"],
+                    labels=x["label"]
+                )
